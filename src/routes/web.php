8e9fb58adcf9c0e8ee14aceb09f4e32e58385dd5
Japan-ai <ai.1110.small.pond@gmail.com>
2020-04-07 21:25:16 +0900

    authority for cart page

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,5 +17,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'ShopController@index');
-Route::get('/mycart', 'ShopController@myCart');
+Route::get('/mycart', 'ShopController@myCart')->middleware('auth');
 
