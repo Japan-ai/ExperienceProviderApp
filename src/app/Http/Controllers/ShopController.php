224db22b2aa0f2ecf8b98581dd715a093acbcb43
Japan-ai <ai.1110.small.pond@gmail.com>
2020-04-08 17:04:42 +0900

    function for adding cart

--- src/app/Http/Controllers/ShopController.php
+++ src/app/Http/Controllers/ShopController.php
@@ -16,26 +16,21 @@ class ShopController extends Controller
     return view('shop',compact('stocks'));
   }
 
-  public function myCart()
-  {
-    $my_carts = Cart::all();
-    //$my_carts変数も引渡す
-    return view('mycart',compact('my_carts'));
-  }
+public function myCart(Cart $cart)
+{
+  $my_carts = $cart->showCart();
+  return view('mycart',compact('my_carts'));
+}
 
-  public function addMycart(Request $request)
-   {
-    $user_id = Auth::id(); 
-    $stock_id=$request->stock_id;
-    $cart_add_info=Cart::firstOrCreate(['stock_id' => $stock_id,'user_id' => $user_id]);
-    
-    if($cart_add_info->wasRecentlyCreated){
-      $message = 'カートに追加しました';
-    }
-    else{
-      $message = 'カートに登録済みです';
-    }
-    $my_carts = Cart::where('user_id',$user_id)->get();
-      return view('mycart',compact('my_carts' , 'message'));
-   }
+public function addMycart(Request $request,Cart $cart)
+{
+  //カートへ追加する処理
+  $stock_id=$request->stock_id;
+  $message = $cart->addCart($stock_id);
+
+  //追加後の情報を取得
+  $my_carts = $cart->showCart();
+
+  return view('mycart',compact('my_carts' , 'message'));
   }
+}
