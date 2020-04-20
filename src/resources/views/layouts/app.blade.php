9f63ecea95a4769fea32cdd3d80bfe033f6cdc81
Japan-ai <ai.1110.small.pond@gmail.com>
2020-04-20 10:05:09 +0900

    bug fix

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -43,15 +43,15 @@
                                </li>
                            @endif
                        @else
-                           <li class="nav-item dropdown">
-                               <a id="navbarDropdown" style="color:#fefefe;" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                   {{ Auth::user()->name }} <span class="caret"></span>
+                       <li class="nav-item dropdown">
+                               <a id="navbarDropdown" class="nav-link dropdown-toggle" style="color:#fefefe;" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   {{ Auth::user()->name }}さん<span class="caret"></span>
                                </a>
 
                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
+                                      onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                        {{ __('ログアウト') }}
                                    </a>
 
