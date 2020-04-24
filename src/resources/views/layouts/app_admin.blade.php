0dcb80464008ec071cd8e820b6241ec35adf3b00
Japan-ai <ai.1110.small.pond@gmail.com>
2020-04-24 14:34:06 +0900

    admin login

---
+++ src/resources/views/layouts/app_admin.blade.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- CSRF Token -->
+   <meta name="csrf-token" content="{{ csrf_token() }}">
+   <title>{{ config('app.name', 'Laravel') }}</title>
+   <!-- Scripts -->
+   <script src="{{ asset('js/app.js') }}" defer></script>
+   <!-- Fonts -->
+   <link rel="dns-prefetch" href="//fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+   <!-- Styles -->
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+   <div id="app" style="background-color:#2c9bfc">
+       <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color:rgb(250, 176, 17); color:#fefefe;">
+           <div class="container">
+               <a class="navbar-brand" style="color:#fefefe; font-size:1.4em" href="{{ url('/') }}" >
+                   {{ config('app.name', 'Laravel') }}
+               </a>
+               <button class="navbar-toggler" type="button"  style="border-color:#fefefe" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                   <span class="navbar-toggler-icon" style="color:#fefefe"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                   <!-- Left Side Of Navbar -->
+                   <ul class="navbar-nav mr-auto">
+
+                   </ul>
+
+                   <!-- Right Side Of Navbar -->
+                   <ul class="navbar-nav ml-auto" >
+                       <!-- Authentication Links -->
+                       @guest
+                           <li class="nav-item">
+                               <a class="nav-link" style="color:#fefefe;"  href="{{ route('admin.login') }}">{{ __('ログイン') }}</a>
+                           </li>
+                           @if (Route::has('register'))
+                               <li class="nav-item">
+                                   <a class="nav-link" style="color:#fefefe;"  href="{{ route('admin.register') }}">{{ __('管理者登録') }}</a>
+                               </li>
+                           @endif
+                       @else
+                       <li class="nav-item dropdown">
+                               <a id="navbarDropdown" class="nav-link dropdown-toggle" style="color:#fefefe;" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   管理者{{ Auth::user()->name }}さん<span class="caret"></span>
+                               </a>
+
+                               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                   <a class="dropdown-item" href="{{ route('admin.logout') }}"
+                                      onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                       {{ __('ログアウト') }}
+                                   </a>
+
+                                   <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+                                       @csrf
+                                   </form>
+                                   <a class="dropdown-item" href="{{ url('/mycart') }}">
+                                       カートを見る
+                                   </a>
+                               </div>
+                           </li>
+                           <a href="{{ url('/mycart') }}" >
+                               <img src="{{ asset('image/cart.png') }}" class="cart" >
+                           </a>
+                       @endguest
+                   </ul>
+               </div>
+           </div>
+       </nav>
+
+       <main class="py-0" style="background-color:#26263c;">
+           @yield('content')
+       </main>
+       <footer class="footer_design" style="background-color:rgb(250, 176, 17); color:#fefefe;" >
+
+       @guest
+           <p class="nav-item" style="display:inline;">
+               <a class="nav-link" href="{{ route('admin.login') }}" style="color:#fefefe; display:inline;">{{ __('ログイン') }}</a>
+           @if (Route::has('register'))
+                   <a class="nav-link" href="{{ route('admin.register') }}" style="color:#fefefe; display:inline;">{{ __('管理者登録') }}</a>
+               </p>
+           @endif  
+       @endguest
+       <br>
+       <div style="margin-top:24px;">
+       Find your dream the most in the world<br>
+       <p style="font-size:2.4em">Experience Provider Shop</p><br>
+       </div>
+       <p style="font-size:0.7em;">@copyright@laravel</p>
+   </footer>
+   </div>
+</body>
+</html>
