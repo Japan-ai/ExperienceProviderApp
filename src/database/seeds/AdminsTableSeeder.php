679f595f81e38d669bba84d44bc872cb710cb047
Japan-ai <ai.1110.small.pond@gmail.com>
2020-05-17 16:12:11 +0900

    debag

--- src/database/seeds/AdminsTableSeeder.php
+++ src/database/seeds/AdminsTableSeeder.php
@@ -7,9 +7,9 @@ class AdminsTableSeeder extends Seeder
     public function run()
     {
         DB::table('admins')->insert([
-            'name' => 'admin',
-            'email' => 'admin@email.com',
-            'password' => bcrypt('admin1234'),
+            'name' => 'admins',
+            'email' => 'admintest@email.com',
+            'password' => bcrypt('admin1110'),
             'remember_token' => Str::random(10),
         ]);
     }
--- src/database/seeds/UsersTableSeeder.php
+++ src/database/seeds/UsersTableSeeder.php
@@ -9,9 +9,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'test',
-            'email' => 'dummy@email.com',
-            'password' => bcrypt('test1234'),
+            'name' => 'tests',
+            'email' => 'dummytest@email.com',
+            'password' => bcrypt('test1110'),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
